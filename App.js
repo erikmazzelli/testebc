6acb3f9dfc25dd28141594c47ee8b5a8c7f248fb
erikmazzelli <erik.mazzelli@conaut.com.br>
2022-03-17 14:33:56 -0300

    implemented logger

--- App.js
+++ App.js
@@ -13,109 +13,108 @@ import {
   SafeAreaView,
   Text,
   LogBox,
-  PermissionsAndroid
+  BackHandler
 } from 'react-native';
-import RNBluetoothClassic, {
-  BluetoothDevice,
-  BluetoothEventType
-} from 'react-native-bluetooth-classic';
+import RNBluetoothClassic from 'react-native-bluetooth-classic';
 
 LogBox.ignoreLogs(['new']);
 
 const App: () => Node = () => {
-  const [bluetooth, setBluetooth] = useState(false);
+  const [bluetoothEnabled, setBluetoothEnabled] = useState(false);
   const [device, setDevice] = useState('...');
   const [connected, setConnected] = useState(false);
+  const [logEnabled, setTeste] = useState(false);
+  const [data, setData] = useState([]);
 
   useEffect(() => {
     const fetchBluetooth = async () => {
       const isBluetoothAvailable = await RNBluetoothClassic.isBluetoothAvailable();
 
-      if (!isBluetoothAvailable) return setBluetooth('Not Available');
+      if (!isBluetoothAvailable) return setBluetoothEnabled(false);
 
       const state = await RNBluetoothClassic.isBluetoothEnabled();
 
-      setBluetooth(state);
+      setBluetoothEnabled(state);
+
+      if (!state) {
+        try {
+          await RNBluetoothClassic.requestBluetoothEnabled();
+        } catch (e) {
+          if (e.message = 'User did not enable Bluetooth') {
+            return BackHandler.exitApp();
+          }
+
+          console.log(e);
+        }
+      }
     };
 
     fetchBluetooth();
-  }, []);
+  }, [bluetoothEnabled]);
 
   useEffect(() => {
     const fetchDevice = async () => {
-      const bondedDevices = await RNBluetoothClassic.getBondedDevices();
-      let filteredDevice;
+      const pairedDevices = await RNBluetoothClassic.getBondedDevices();
 
-      bondedDevices.forEach(device => {
-        if (device.name === "IFC050P-A832A1974C2") {
-          filteredDevice = device;
-        }
+      const filteredDevice = pairedDevices.find(device => {
+        return device.name === 'Pitometria';
       });
 
       setDevice(filteredDevice);
+
+      if (!filteredDevice) alert(
+        'Pairing with a compatible device is required'
+      );
     };
 
-    bluetooth && fetchDevice();
-  }, [bluetooth]);
+    bluetoothEnabled && fetchDevice();
+  }, [bluetoothEnabled]);
 
   useEffect(() => {
-    const subscription = RNBluetoothClassic.onStateChanged(async (state) => {
-      setBluetooth(state.enabled);
+    const subscription = RNBluetoothClassic.onStateChanged(state => {
+      setBluetoothEnabled(state.enabled);
+
+      if (!state.enabled) {
+        setConnected(state.enabled);
+        setDevice('...');
+      }
     });
 
     return () => subscription.remove();
   }, []);
 
-  const requestAccessFineLocationPermission = async () => {
-    const granted = await PermissionsAndroid.request(
-      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
-      {
-        title: 'Access fine location required for discovery',
-        message:
-          'In order to perform discovery, you must enable/allow' +
-          'fine location access.',
-        buttonNeutral: 'Ask Me Later',
-        buttonNegative: 'Cancel',
-        buttonPositive: 'Ok'
+  useEffect(() => {
+    const interval = setInterval(async () => {
+      const date = new Date;
+
+      try {
+        await device.write('1');
+
+        const response = await device.read();
+
+        return console.log(`
+          Date (HH:MM:SS:Ms): ${date.getHours()}:${date.getMinutes().toLocaleString()}:${date.getSeconds()}:${date.getMilliseconds()}
+          Response: ${response}
+          `);
+
+      } catch (e) {
+        console.log(e);
       }
-    );
+    }, 1000);
 
-    return granted === PermissionsAndroid.RESULTS.GRANTED;
-  };
 
-  const startScan = async () => {
-    try {
-      const granted = await requestAccessFineLocationPermission();
+    if (!logEnabled) return clearInterval(interval);
 
-      if (!granted) return alert('Access fine location was not granted');
-
-      const devices = await RNBluetoothClassic.startDiscovery();
-
-      let deviceList = [];
-
-      devices.forEach((device, index) => {
-        deviceList[index] = `${device.name}`;
-      });
-
-      console.log(deviceList);
-    } catch (e) {
-      console.log(e);
-    }
-  };
-
-  const stopScan = async () => {
-    try {
-      await RNBluetoothClassic.cancelDiscovery();
-    } catch (e) {
-      console.log(e);
-    }
-  };
+    return () => clearInterval(interval);
+  }, [logEnabled]);
 
   const startConnection = async () => {
     try {
-      const response = await device.connect();
-      console.log(`Connection: ${response}`);
-      setConnected(response);
+      if (!connected) {
+        const response = await device.connect();
+        console.log(`Connection: ${response}`);
+        setConnected(response);
+      }
     } catch (e) {
       console.log(e);
     }
@@ -123,11 +122,22 @@ const App: () => Node = () => {
 
   const stopConnection = async () => {
     try {
-      const response = await device.disconnect();
-      console.log(`Disconnection: ${response}`);
-      setConnected(!response);
+      if (connected) {
+        const response = await device.disconnect();
+        console.log(`Disconnection: ${response}`);
+        setConnected(!response);
+      }
     } catch (e) {
       console.log(e);
+    }
+  };
+
+  const startLog = () => {
+    setTeste(!logEnabled);
+    if (!logEnabled) {
+      console.log('Iniciando log');
+    } else {
+      console.log('Encerrando log');
     }
   };
 
@@ -135,26 +145,17 @@ const App: () => Node = () => {
     command = String(command);
 
     try {
-      if (device !== '...' && connected) {
-        await device.write(`AT+${command}\r\n`);
+      await device.write(`AT+${command}?\r\n`);
+      const available = await device.available();
 
-        const response1 = await device.read();
-        const response2 = await device.read();
-
-        console.log(`
-          Response:
-          ${response1}
-          ${response2}
-        `);
-
-        return alert(`
-          Response:
-          ${response1}
-          ${response2}
-        `);
-      } else {
-        return alert('Dispositivo não pareado ou desconectado');
+      if (available > 0) {
+        for (let i = 0; i < available; i++) {
+          let response = await device.read();
+          console.log(i);
+          setData([response, ...data]);
+        }
       }
+      console.log(data);
     } catch (e) {
       console.log(e);
     }
@@ -165,29 +166,36 @@ const App: () => Node = () => {
     value = String(value);
 
     try {
-      if (device !== '...' && connected) {
-        await device.write(`AT+${command}=${value}\r\n`);
-        const response1 = await device.read();
-        const response2 = await device.read();
-        console.log(response1, response2);
-        return alert(response1, response2);
-      } else {
-        return alert('Dispositivo não pareado ou desconectado');
+      await device.write(`AT+${command}=${value}\r\n`);
+      const available = await device.available();
+
+      if (available > 0) {
+        for (let i = 0; i < available; i++) {
+          let data = await device.read();
+          console.log(data);
+        }
       }
     } catch (e) {
       console.log(e);
     }
   };
 
-  const testIFC = async (command) => {
-    try {
-      await device.write(command);
-      const response = await device.read();
-      console.log(response);
-    } catch (e) {
-      console.log(e);
-    }
-  };
+  // const testIFC = async (command) => {
+  //   try {
+  //     await device.write(command);
+
+  //     const available = await device.available();
+
+  //     if (available > 0) {
+  //       for (let i = 0; i < available; i++) {
+  //         const data = await device.read();
+  //         console.log(data);
+  //       }
+  //     }
+  //   } catch (e) {
+  //     console.log(e);
+  //   }
+  // };
 
   return (
     <SafeAreaView
@@ -196,7 +204,7 @@ const App: () => Node = () => {
       <Text style={{ fontSize: 16 }}>
         {
           `
-          Bluetooth enabled: ${bluetooth}
+          Bluetooth enabled: ${bluetoothEnabled}
 
           Paired Device: ${device?.name || '...'}
 
@@ -204,41 +212,36 @@ const App: () => Node = () => {
           `
         }
       </Text>
-      {/* <Button
-        title='Start Scan'
-        onPress={startScan}
-        disabled={bluetooth === 'Not Available'}
-      />
-      <Button
-        title='Stop Scan'
-        onPress={stopScan}
-        disabled={bluetooth === 'Not Available'}
-      /> */}
       <Button
         title='Start Connection'
         onPress={startConnection}
-        disabled={bluetooth === 'Not Available'}
+        disabled={!bluetoothEnabled}
       />
       <Button
         title='Stop Connection'
         onPress={stopConnection}
-        disabled={bluetooth === 'Not Available'}
+        disabled={!bluetoothEnabled}
       />
-      {/* <Button
+      <Button
+        title={logEnabled ? 'Stop Logging' : 'Start Logging'}
+        onPress={() => startLog()}
+        disabled={!bluetoothEnabled}
+      />
+      <Button
         title='Read Action'
-        onPress={() => readAction('MEDIDAS?')}
-        disabled={bluetooth === 'Not Available'}
+        onPress={() => readAction('MEDIDAS')}
+        disabled={!bluetoothEnabled}
       />
       <Button
         title='Write Action'
-        onPress={() => writeAction('DATAHORA', '150320221447000000')}
-        disabled={bluetooth === 'Not Available'}
-      /> */}
-      <Button
-        title='IFC050P'
-        onPress={() => testIFC('3')}
-        disabled={bluetooth === 'Not Available'}
+        onPress={() => writeAction('DATAHORA', '170322073900')}
+        disabled={!bluetoothEnabled}
       />
+      {/* <Button
+        title='IFC050P'
+        onPress={() => testIFC('1')}
+        disabled={!bluetoothEnabled}
+      /> */}
     </SafeAreaView>
   );
 };
